e6c7d499b9c5e5f1173cc30332b0cb51e3f91e5c
Andrew Jones <Andrew.williamson.jones@gmail.com>
2019-01-21 22:51:24 -0600

    Dicey

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -3,3 +3,40 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   // You code here
 });
+var listItems = document.querySelectorAll('li')
+
+var addH2 = document.createElement('h2')
+
+var content = document.createTextNode ('You have ' + listItems.length + ' items in your shopping cart')
+
+addH2.appendChild(content)
+
+var shoppingElement = document.querySelector('h1')
+
+shoppingElement.appendChild(addH2)
+
+var listInput = document.createElement ('input')
+
+var inputDiv = document.createElement('div')
+
+var listInput = document.createElement('input')
+inputDiv.appendChild(listInput)
+
+var h2Element = document.querySelector('h2')
+h2Element.appendChild(inputDiv)
+
+var button = document.createElement('button')
+inputDiv.appendChild(button)
+
+var buttonText = document.createTextNode('Add List Item')
+button.appendChild(buttonText)
+button.appendChild(buttonText)
+
+button.onclick = 'addListItem()'
+function addListItem () {
+  var getInput = document.querySelector;
+  var inputValue = getInput.inputValue
+  var ulList = document.createElement
+  console.log('hello!')
+  button.setAttribute("onclick", 'addListItem');
+}
